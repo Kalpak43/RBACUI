27f0485956409378155b5cd856ea6fb39ba8090e
Kalpak Goshikwar <kalpakgoshikwar123@gmail.com>
2024-11-27 07:23:42 +0530

    Updated login form

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This is a simple UI for managing user roles and permissions in a system. It is b
 ## Installation
 
 1. Clone the repository
-2. Run `npm install --legacy-peer-deps` (used --legacy-peer-deps because shadcn does not support latest versions of Nextjs and React) to install the dependencies.
+2. Run `npm install --legacy-peer-deps` (used --legacy-peer-deps as most libraries do not support React 19 as of yet) to install the dependencies.
 3. Run `npm run dev` to start the development server.
 
 ## Features
@@ -15,10 +15,29 @@ This is a simple UI for managing user roles and permissions in a system. It is b
 4. **Users Page**: A page to manage users in the system. You can add, edit and delete users. Has options to add users from a CSV file. Also, lets you filter users based on roles and status.(Accessible to only admin and editor role)
 5. **Permissions Page**: Lets you manage permissions. You can add permissions based on roles or assign permissions to users directly. (Accessible to only admin role)
 
+## Mock API
+1. **/api/login**: Mock API for login. Accepts email and password and returns user details.
+2. **/api/all-users**: Mock API for getting list of all users.
+3. **/api/update-user**: Mock API for updating user info. If user is not found, it creates a new user.
+4. **/api/upload-csv**: Mock API for uploading a CSV file to add users.
+5. **/api/permissions**: Mock API for getting permissions.
+6. **/api/get-numbers**: Mock API for getting the number of users by roles, status and permissions in the system.
+
 ## Tech Stack
 1. **React**: Frontend library for building user interfaces.
 2. **Nextjs**: React framework for building server-side rendered applications.
 3. **Shadcn**: A simple UI library for React.
 4. **Next Serverless Functions**: Used for creatign mock API requests.
+
+## Login Credentials (For testing)
+1. **Admin**:
+    - Email: admin@email.com
+    - Password: admin
+2. **Editor**:
+    - Email: editor1@email.com
+    - Password: editor1
+3. **User**:
+    - Email: user1@email.com
+    - Password: user1
 
 
--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,49 +1,91 @@
 "use client";
 
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useAuth } from "@/hooks/AuthProvider";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(4, "Password must be at least 4 characters"),
+});
+
+type FormValues = z.infer<typeof formSchema>;
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
+  const [error, setError] = useState<string | null>(null);
   const { login } = useAuth();
 
-  const handleLogin = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    await login(email, password);
+  const handleLogin = async (values: FormValues) => {
+    try {
+      await login(values.email, values.password);
+    } catch (err) {
+      setError("Invalid email or password");
+    }
   };
 
   return (
     <Card className="w-full max-w-md">
       <CardHeader>
-        <CardTitle>Login</CardTitle>
+        <CardTitle className="text-center">
+          <h1 className="text-2xl">Login</h1>
+        </CardTitle>
       </CardHeader>
       <CardContent>
-        <form onSubmit={handleLogin} className="space-y-4">
-          <Input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-            required
-          />
-          <Input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            required
-          />
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-          {/* {error && <p className="text-red-500 text-center">{error}</p>} */}
-        </form>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(handleLogin)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+            {error && <p className="text-red-500 text-center">{error}</p>}
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
